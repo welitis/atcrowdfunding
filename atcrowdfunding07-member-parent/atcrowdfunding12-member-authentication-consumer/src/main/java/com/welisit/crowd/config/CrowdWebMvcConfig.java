91bdcd8a196ffa758f376e809d6635f3bf050cac
王强 <943446906@qq.com>
2020-06-11 13:46:51 +0800

    更改首页导航栏显示用户判断

--- atcrowdfunding07-member-parent/atcrowdfunding12-member-authentication-consumer/src/main/java/com/welisit/crowd/config/CrowdWebMvcConfig.java
+++ atcrowdfunding07-member-parent/atcrowdfunding12-member-authentication-consumer/src/main/java/com/welisit/crowd/config/CrowdWebMvcConfig.java
@@ -21,6 +21,7 @@ public class CrowdWebMvcConfig implements WebMvcConfigurer {
 		registry.addViewController("/auth/member/to/login/page").setViewName("member-login");
 		registry.addViewController("/auth/member/to/center/page").setViewName("member-center");
 		registry.addViewController("/member/my/crowd").setViewName("member-crowd");
+		registry.addViewController("/bbb").setViewName("bbb");
 	}
 
 }
